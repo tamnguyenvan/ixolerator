70af3a54fb4d0a2c2633ae769f5ecf1a0a10ad17
Nguyen Van Tam <tamnvhustcc@gmail.com>
2022-12-07 10:00:30 +0700

    Import from yolo repos. Added training pipeline (not runnable)

---
+++ meghnad/core/cv/obj_det/src/pytorch/train/train.py
@@ -0,0 +1,317 @@
+import time
+import os
+import math
+import sys
+from typing import List, Tuple, Union, Callable, Dict
+
+import numpy as np
+import tensorflow as tf
+
+from meghnad.core.cv.obj_det.src.pytorch.data_loader import PytorchObjDetDataLoader
+from meghnad.core.cv.obj_det.cfg import ObjDetConfig
+
+from utils import ret_values
+from utils.log import Log
+from utils.common_defs import class_header, method_header
+
+from meghnad.core.cv.obj_det.src.pytorch.train.select_model import PytorchObjDetSelectModel
+# from meghnad.core.cv.obj_det.src.pytorch.train.eval import PytorchObjDetEval
+# from meghnad.core.cv.obj_det.src.pytorch.train.train_utils import get_optimizer
+
+
+__all__ = ['TFObjDetTrn']
+
+
+log = Log()
+
+
+@method_header(
+    description="""Returns configs from given settings
+
+    Parameters
+    ----------
+    settings : List[str]
+        A list of string represents settings.
+
+    Returns
+    -------
+    [model_cfgs]
+        A list of string represents corresponding model configs
+    """)
+def load_config_from_settings(settings: List[str]) -> Tuple[List, List]:
+
+    settings = [f'{setting}_models' for setting in settings]
+    cfg_obj = ObjDetConfig()
+    data_cfg = cfg_obj.get_data_cfg()
+
+    model_cfgs = []
+    data_cfgs = []
+    for setting in settings:
+        model_names = cfg_obj.get_model_settings(setting)
+        for model_name in model_names:
+            model_cfg = cfg_obj.get_model_cfg(model_name)
+            model_cfgs.append(model_cfg)
+            data_cfgs.append(data_cfg)
+    return model_cfgs, data_cfgs
+
+
+@class_header(
+    description='''
+        Class for object detection model training''')
+class PytorchObjDetTrn:
+    def __init__(self, settings: List[str]) -> None:
+        self.settings = settings
+        self.model_cfgs, self.data_cfgs = load_config_from_settings(settings)
+        self.model_selection = PytorchObjDetSelectModel(self.model_cfgs)
+        self.data_loaders = []
+        self.best_model_path = None
+
+    @method_header(
+        description='''
+                Helper for configuring data connectors.''',
+        arguments='''
+                data_path: location of the training data (should point to the file in case of a single file, should point to
+                the directory in case data exists in multiple files in a directory structure)
+                ''')
+    def config_connectors(self, data_path: str, augmentations: Dict = None) -> None:
+        self.data_loaders = [PytorchObjDetDataLoader(data_path, data_cfg, model_cfg, augmentations)
+                             for data_cfg, model_cfg in zip(self.data_cfgs, self.model_cfgs)]
+
+    @method_header(
+        description='''
+                Function to set training configurations and start training.''',
+        arguments='''
+                epochs: set epochs for the training by default it is 10
+                checkpoint_dir: directory from where the checkpoints should be loaded
+                logdir: directory where the logs should be saved
+                resume_path: The path/checkpoint from where the training should be resumed
+                print_every: an argument to specify when the function should print or after how many epochs
+                ''')
+    def train(self,
+              epochs: int = 10,
+              checkpoint_dir: str = './checkpoints',
+              logdir: str = './training_logs',
+              resume_path: str = None,
+              print_every: int = 10,
+              **hyp) -> Tuple:
+        try:
+            epochs = int(epochs)
+            if epochs <= 0:
+                log.ERROR(sys._getframe().f_lineno,
+                          __file__, __name__, "Epochs value must be a positive integer")
+                return ret_values.IXO_RET_INVALID_INPUTS
+
+        except ValueError:
+            log.ERROR(sys._getframe().f_lineno,
+                      __file__, __name__, "Epochs value must be a positive integer")
+            return ret_values.IXO_RET_INVALID_INPUTS
+
+        # batch -------------------------------------------------------------
+        for i, (imgs, targets, paths, _) in pbar:
+            # number integrated batches (since train start)
+            ni = i + nb * epoch
+            imgs = imgs.to(device, non_blocking=True).float() / \
+                255.0  # uint8 to float32, 0-255 to 0.0-1.0
+
+            # Warmup
+            if ni <= nw:
+                xi = [0, nw]  # x interp
+                # model.gr = np.interp(ni, xi, [0.0, 1.0])  # iou loss ratio (obj_loss = 1.0 or iou)
+                accumulate = max(1, np.interp(
+                    ni, xi, [1, nbs / total_batch_size]).round())
+                for j, x in enumerate(optimizer.param_groups):
+                    # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
+                    x['lr'] = np.interp(
+                        ni, xi, [hyp['warmup_bias_lr'] if j == 2 else 0.0, x['initial_lr'] * lf(epoch)])
+                    if 'momentum' in x:
+                        x['momentum'] = np.interp(
+                            ni, xi, [hyp['warmup_momentum'], hyp['momentum']])
+
+            # Multi-scale
+            if opt.multi_scale:
+                sz = random.randrange(
+                    imgsz * 0.5, imgsz * 1.5 + gs) // gs * gs  # size
+                sf = sz / max(imgs.shape[2:])  # scale factor
+                if sf != 1:
+                    # new shape (stretched to gs-multiple)
+                    ns = [math.ceil(x * sf / gs) * gs for x in imgs.shape[2:]]
+                    imgs = F.interpolate(
+                        imgs, size=ns, mode='bilinear', align_corners=False)
+
+            # Forward
+            with amp.autocast(enabled=cuda):
+                pred = model(imgs)  # forward
+                if 'loss_ota' not in hyp or hyp['loss_ota'] == 1:
+                    loss, loss_items = compute_loss_ota(pred, targets.to(
+                        device), imgs)  # loss scaled by batch_size
+                else:
+                    loss, loss_items = compute_loss(
+                        pred, targets.to(device))  # loss scaled by batch_size
+                if rank != -1:
+                    loss *= opt.world_size  # gradient averaged between devices in DDP mode
+                if opt.quad:
+                    loss *= 4.
+
+            # Backward
+            scaler.scale(loss).backward()
+
+            # Optimize
+            if ni % accumulate == 0:
+                scaler.step(optimizer)  # optimizer.step
+                scaler.update()
+                optimizer.zero_grad()
+                if ema:
+                    ema.update(model)
+
+            # Print
+            if rank in [-1, 0]:
+                mloss = (mloss * i + loss_items) / \
+                    (i + 1)  # update mean losses
+                mem = '%.3gG' % (torch.cuda.memory_reserved(
+                ) / 1E9 if torch.cuda.is_available() else 0)  # (GB)
+                s = ('%10s' * 2 + '%10.4g' * 6) % (
+                    '%g/%g' % (epoch, epochs - 1), mem, *mloss, targets.shape[0], imgs.shape[-1])
+                pbar.set_description(s)
+
+                # Plot
+                if plots and ni < 10:
+                    f = save_dir / f'train_batch{ni}.jpg'  # filename
+                    Thread(target=plot_images, args=(
+                        imgs, targets, paths, f), daemon=True).start()
+                    # if tb_writer:
+                    #     tb_writer.add_image(f, result, dataformats='HWC', global_step=epoch)
+                    #     tb_writer.add_graph(torch.jit.trace(model, imgs, strict=False), [])  # add model graph
+                elif plots and ni == 10 and wandb_logger.wandb:
+                    wandb_logger.log({"Mosaics": [wandb_logger.wandb.Image(str(x), caption=x.name) for x in
+                                                  save_dir.glob('train*.jpg') if x.exists()]})
+
+            # end batch ------------------------------------------------------------------------------------------------
+        # end epoch ----------------------------------------------------------------------------------------------------
+
+        # Scheduler
+        lr = [x['lr'] for x in optimizer.param_groups]  # for tensorboard
+        scheduler.step()
+
+        # DDP process 0 or single-GPU
+        if rank in [-1, 0]:
+            # mAP
+            ema.update_attr(
+                model, include=['yaml', 'nc', 'hyp', 'gr', 'names', 'stride', 'class_weights'])
+            final_epoch = epoch + 1 == epochs
+            if not opt.notest or final_epoch:  # Calculate mAP
+                wandb_logger.current_epoch = epoch + 1
+                results, maps, times = test.test(data_dict,
+                                                 batch_size=batch_size * 2,
+                                                 imgsz=imgsz_test,
+                                                 model=ema.ema,
+                                                 single_cls=opt.single_cls,
+                                                 dataloader=testloader,
+                                                 save_dir=save_dir,
+                                                 verbose=nc < 50 and final_epoch,
+                                                 plots=plots and final_epoch,
+                                                 wandb_logger=wandb_logger,
+                                                 compute_loss=compute_loss,
+                                                 is_coco=is_coco,
+                                                 v5_metric=opt.v5_metric)
+
+            # Write
+            with open(results_file, 'a') as f:
+                # append metrics, val_loss
+                f.write(s + '%10.4g' * 7 % results + '\n')
+            if len(opt.name) and opt.bucket:
+                os.system('gsutil cp %s gs://%s/results/results%s.txt' %
+                          (results_file, opt.bucket, opt.name))
+
+            # Log
+            tags = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
+                    'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',
+                    'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
+                    'x/lr0', 'x/lr1', 'x/lr2']  # params
+            for x, tag in zip(list(mloss[:-1]) + list(results) + lr, tags):
+                if tb_writer:
+                    tb_writer.add_scalar(tag, x, epoch)  # tensorboard
+                if wandb_logger.wandb:
+                    wandb_logger.log({tag: x})  # W&B
+
+            # Update best mAP
+            # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
+            fi = fitness(np.array(results).reshape(1, -1))
+            if fi > best_fitness:
+                best_fitness = fi
+            wandb_logger.end_epoch(best_result=best_fitness == fi)
+
+            # Save model
+            if (not opt.nosave) or (final_epoch and not opt.evolve):  # if save
+                ckpt = {'epoch': epoch,
+                        'best_fitness': best_fitness,
+                        'training_results': results_file.read_text(),
+                        'model': deepcopy(model.module if is_parallel(model) else model).half(),
+                        'ema': deepcopy(ema.ema).half(),
+                        'updates': ema.updates,
+                        'optimizer': optimizer.state_dict(),
+                        'wandb_id': wandb_logger.wandb_run.id if wandb_logger.wandb else None}
+
+                # Save last, best and delete
+                torch.save(ckpt, last)
+                if best_fitness == fi:
+                    torch.save(ckpt, best)
+                if (best_fitness == fi) and (epoch >= 200):
+                    torch.save(ckpt, wdir / 'best_{:03d}.pt'.format(epoch))
+                if epoch == 0:
+                    torch.save(ckpt, wdir / 'epoch_{:03d}.pt'.format(epoch))
+                elif ((epoch + 1) % 25) == 0:
+                    torch.save(ckpt, wdir / 'epoch_{:03d}.pt'.format(epoch))
+                elif epoch >= (epochs - 5):
+                    torch.save(ckpt, wdir / 'epoch_{:03d}.pt'.format(epoch))
+                if wandb_logger.wandb:
+                    if ((epoch + 1) % opt.save_period == 0 and not final_epoch) and opt.save_period != -1:
+                        wandb_logger.log_model(
+                            last.parent, opt, epoch, fi, best_model=best_fitness == fi)
+                del ckpt
+
+        # end epoch ----------------------------------------------------------------------------------------------------
+    # end training
+    if rank in [-1, 0]:
+        # Plots
+        if plots:
+            plot_results(save_dir=save_dir)  # save as results.png
+            if wandb_logger.wandb:
+                files = ['results.png', 'confusion_matrix.png', *
+                         [f'{x}_curve.png' for x in ('F1', 'PR', 'P', 'R')]]
+                wandb_logger.log({"Results": [wandb_logger.wandb.Image(str(save_dir / f), caption=f) for f in files
+                                              if (save_dir / f).exists()]})
+        # Test best.pt
+        logger.info('%g epochs completed in %.3f hours.\n' %
+                    (epoch - start_epoch + 1, (time.time() - t0) / 3600))
+        if opt.data.endswith('coco.yaml') and nc == 80:  # if COCO
+            for m in (last, best) if best.exists() else (last):  # speed, mAP tests
+                results, _, _ = test.test(opt.data,
+                                          batch_size=batch_size * 2,
+                                          imgsz=imgsz_test,
+                                          conf_thres=0.001,
+                                          iou_thres=0.7,
+                                          model=attempt_load(m, device).half(),
+                                          single_cls=opt.single_cls,
+                                          dataloader=testloader,
+                                          save_dir=save_dir,
+                                          save_json=True,
+                                          plots=False,
+                                          is_coco=is_coco,
+                                          v5_metric=opt.v5_metric)
+
+        # Strip optimizers
+        final = best if best.exists() else last  # final model
+        for f in last, best:
+            if f.exists():
+                strip_optimizer(f)  # strip optimizers
+        if opt.bucket:
+            os.system(f'gsutil cp {final} gs://{opt.bucket}/weights')  # upload
+        if wandb_logger.wandb and not opt.evolve:  # Log the stripped model
+            wandb_logger.wandb.log_artifact(str(final), type='model',
+                                            name='run_' + wandb_logger.wandb_run.id + '_model',
+                                            aliases=['last', 'best', 'stripped'])
+        wandb_logger.finish_run()
+    else:
+        dist.destroy_process_group()
+    torch.cuda.empty_cache()
+    return results
